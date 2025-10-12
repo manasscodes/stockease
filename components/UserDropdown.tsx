aac779040ecd925f16dfca7d76625f8a68590863
codewmanas <manas.kolaskar@somaiya.edu>
2025-10-12 15:51:04 +0530

    Implement Search Stocks

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -14,7 +14,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import NavItems from "./NavItems";
 import { signOut } from "@/lib/actions/auth.actions";
 
-const UserDropdown = ({ user }:{user:User}) => {
+const UserDropdown = ({ user, initialStocks }:{user:User, initialStocks:StockWithWatchlistStatus[]}) => {
   const router = useRouter();
 
   const handleSignOut = async () => {
@@ -68,7 +68,7 @@ const UserDropdown = ({ user }:{user:User}) => {
           </DropdownMenuItem>
           <DropdownMenuSeparator className=" hidden sm:block bg-gray-600" />
           <nav className="sm:hidden">
-               <NavItems />
+               <NavItems initialStocks={initialStocks} />
           </nav>
       </DropdownMenuContent>
     </DropdownMenu>
